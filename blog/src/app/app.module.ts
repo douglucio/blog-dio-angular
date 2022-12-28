2298defd68bedf256fd78d6fdfb1579d7d81af33
douglucio <ronaldoreis.web@gmail.com>
2022-12-28 15:21:24 -0300

    Carregando componentes iniciais do projeto

---
+++ blog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigcardComponent } from './components/bigcard/bigcard.component';
+import { SmallcardComponent } from './components/smallcard/smallcard.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigcardComponent,
+    SmallcardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ blog/src/app/components/bigcard/bigcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bigcard',
+  templateUrl: './bigcard.component.html',
+  styleUrls: ['./bigcard.component.css']
+})
+export class BigcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
